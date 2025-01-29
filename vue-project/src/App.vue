2a25a55858921e08e20df7d1b87872aff12d5801
Charlemagne De Guzman <csdeguzman@student.apc.edu.ph>
2025-01-29 23:45:46 +0800

    commit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,19 @@
+<script></script>
+
 <template>
-  <h1>Food</h1>
-  <personal-profile> <br>
-  <food-item/>  <br>
-  <food-item2/> <br>
-  <food-item/>  <br>
-  <food-item2/> <br>
+   <h1>Food</h1>
+  <personal-profile/> <br>
+  <food-item/> 
+  <food-item2/>
+  <food-item/> 
+  <food-item2/> 
 </template>
 
-<script></script>
+<style></style>
+
+
+
+
+
+
+

da2431297ce26da30d4a4f21a2658251d00eed3b
Charlemagne De Guzman <csdeguzman@student.apc.edu.ph>
2025-01-29 23:53:33 +0800

    pushhh

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -3,13 +3,12 @@
         <header> <h1>Personal Profile Web page </h1>  </header>
         <main>
           <section class="main-content">
-            <div>1 About Me</div>
-            <div>2 Education / Achievements</div>
-            <div>3 Course </div>  
-            <div>4 IT Experience</div>
-            <div>5 Hobbies & Interests</div>
-            <div>6 Goals In Life / Dream</div>  
-            <div>7 Picture Gallery</div>
+            <div>1 About Me: Hello! My name is Charlemagne, but you can call me Cha! I am passionate about discovering new things and learning from it.</div>
+            <div>2 Education / Achievements: Senior High School - HUMSS Strand - Pateros Catholic School</div>
+            <div>3 Course: I am currently pursuing BSIT-MI </div>  
+            <div>4 IT Experience: Learned programming languages like Python and Java in my past subjects.</div>
+            <div>5 Hobbies & Interests: In my free time, I like to meditate, explore makeup, and cook for the people I love. I enjoy taking time to relax and recharge.</div>
+            <div>6 Goals In Life / Dream: My ultimate goal is to provide for my family and repay them for all their hard work and sacrifices.</div>  
           </section>
           <aside class="sidebar">
             </aside>

18b824436b0c94aa83a2076549a7ae6ec3fa1089
Charlemagne De Guzman <csdeguzman@student.apc.edu.ph>
2025-01-29 23:39:18 +0800

    pushh

---
+++ vue-project/src/components/FoodItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="apples">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Apples',
+      message: 'I like apples'
+    }
+  }
+};
+</script>
+
+<style>
+  .apples{
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
